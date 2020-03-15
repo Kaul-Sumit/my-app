7961f40e74bc15a74dc45dd27beae9be46dd7934
Sumit Kaul <sumit.kaul@coupang.com>
2020-03-14 17:57:58 -0700

    initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,64 @@
+package com.mycompany.app;
+
+///**
+// * Hello world!
+// *
+// */
+//public class App 
+//{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+//}
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+
+	// http://localhost:8080/RESTfulExample/json/product/get
+	public static void main(String[] args) {
+
+	  try {
+
+		URL url = new URL("http://34.70.62.138/artifactory/api/storage/libs-snapshot-local/org/jfrog/test/multi1/6.88-SNAPSHOT/multi1-6.88-20190422.230806-1-tests.jar?stats");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	  }
+
+	}
+
+}

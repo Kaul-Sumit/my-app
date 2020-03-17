ecb7c77b8eab9467ede67221cf3882ed579396a4
Sumit Kaul <sumit.kaul.87@gmail.com>
2020-03-16 18:06:34 -0700

    cleaning the app.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,5 @@
 package com.mycompany.app;
 
-///**
-// * Hello world!
-// *
-// */
-//public class App 
-//{
-//    public static void main( String[] args )
-//    {
-//        System.out.println( "Hello World!" );
-//    }
-//}
-
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,17 +8,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import org.json.simple.JSONObject;
 
-
-
 public class App {
-
 	
 	public static void main(String[] args) {
 
-	  try {
-
-		//URL url = new URL("http://34.70.62.138/artifactory/api/storage/libs-snapshot-local/org/jfrog/test/multi1/6.88-SNAPSHOT/multi1-6.88-20190422.230806-1-tests.jar?stats");
-		
+	  try {		
 		URL url = new URL("http://34.70.62.138/artifactory/api/search/artifact?name=*jar&repos=jcenter-cache");		
   
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -48,20 +28,12 @@ public class App {
 			(conn.getInputStream()))); 
 
 		Object output;
-		
-		//JSONObject jsonObject = (JSONObject) output;
 		System.out.println("Output from Server .... \n");
 		while ((output = br.readLine()) != null) {
 				
 			System.out.println(output);
 		}
-		
-		
-		
-		
-		
-		
-
+	
 		conn.disconnect();
 
 	  } catch (MalformedURLException e) {

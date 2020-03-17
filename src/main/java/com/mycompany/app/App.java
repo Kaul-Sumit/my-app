7b4aa75f42aa4aa9245d34e4521126eb1bf513b2
Sumit Kaul <sumit.kaul@coupang.com>
2020-03-16 18:05:06 -0700

    cleaning up unused import

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import org.json.simple.JSONObject;
 
 public class App {
 	
